31863cd551e48ca19288e27a9ec01b0a7adda8d6
SilverCatKawaii <mikanmc77772@gmail.com>
2024-10-18 14:36:09 +0900

    mention 別irc

--- src/main/java/net/shoreline/client/impl/module/client/IRCModule.java
+++ src/main/java/net/shoreline/client/impl/module/client/IRCModule.java
@@ -1,32 +1,20 @@
 package net.shoreline.client.impl.module.client;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import net.minecraft.util.Formatting;
-import net.shoreline.client.OvaqReborn;
 import net.shoreline.client.api.config.Config;
 import net.shoreline.client.api.config.setting.BooleanConfig;
-import net.shoreline.client.api.event.EventStage;
 import net.shoreline.client.api.event.listener.EventListener;
 import net.shoreline.client.api.module.ModuleCategory;
 import net.shoreline.client.api.module.ToggleModule;
-import net.shoreline.client.impl.event.TickEvent;
 import net.shoreline.client.impl.event.gui.chat.ChatMessageEvent;
 import net.shoreline.client.impl.event.network.SocketReceivedPacketEvent;
 import net.shoreline.client.socket.SocketChat;
 import net.shoreline.client.socket.SocketWebhookManager;
 import net.shoreline.client.util.chat.ChatUtil;
-import org.jetbrains.annotations.NotNull;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author Rom & Hypinohaizin
@@ -41,6 +29,18 @@ public class IRCModule extends ToggleModule {
             "7wp5",
             "Naa_Naa"
     };
+    Map<String, String> map = Stream.of(new String[][] {
+            { "@hypinohaizin", "<@1143899002463588423>"},
+            { "@rom", "<@904534958901723146>" },
+            { "@naanaa", "<@1187645156338442250>" },
+            { "@momo", "<@1258293421290815488>" },
+            { "@taketyan", "<@1162993757285781624>" },
+            { "@asupara", "<@1076669435022164109>" },
+            { "@tikuwa", "<@1260213569723699210>" },
+            { "@smoky", "<@875378998744580186>" },
+            { "@ken", "<@1072488642817294389>" },
+            { "@dacho", "<@1113048267136176169>" }
+    }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
 
     private String lastMessageId = null;
 
@@ -70,91 +70,11 @@ public class IRCModule extends ToggleModule {
         }).start();
     }
 
-    @EventListener
-    public void onTick(TickEvent event) {
-        if (event.getStage() != EventStage.PRE) {
-            return;
+    private String replaceMentions(String content) {
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            content = content.replace(entry.getKey(), entry.getValue());
         }
-        if (discordConfig.getValue() && receivediscordConfig.getValue()) {
-            new Thread(() -> {
-                try {
-                    String myId = "1296263865327288403";
-
-                    new Thread(() -> {
-                        try {
-                            URL url = new URL("https://discord.com/api/v9/channels/" + myId + "/messages?limit=1");
-                            final String jsonResponse = getString(url);
-                            if (jsonResponse != null && !jsonResponse.isEmpty()) {
-                                List<Message> messages = parseMessages(jsonResponse);
-                                if (!messages.isEmpty() && !messages.get(0).id().equals(lastMessageId)) {
-                                    lastMessageId = messages.get(0).id();
-                                    if (!isBotOrWebhook(messages.get(0))) {
-                                        ChatUtil.clientSendMessageRaw(Formatting.GRAY + "[" + Formatting.AQUA + "OvaqReborn" + Formatting.DARK_BLUE + "Discord" + Formatting.GRAY + "] " + Formatting.WHITE + Formatting.BOLD + messages.get(0).author() + ": " + messages.get(0).content() + Formatting.RESET);
-                                    }
-                                }
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        } finally {
-                            try {
-                                Thread.sleep(5000);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }).start();
-                } catch (Exception e) {
-                }
-            }).start();
-        }
-    }
-
-    private @NotNull String getString(URL url) throws IOException {
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("Authorization", "MTI5NjYzNzgxNDk4NDY3NTQyMw.Gk9uJ7.QcH-4CWG484hqs3mIzSLtrwd0uN94XBE0HYrM8");
-        try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuilder content = new StringBuilder();
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-            }
-            in.close();
-            con.disconnect();
-            return content.toString();
-        } catch (IOException e) {
-            if (con.getResponseCode() == 401) {
-                OvaqReborn.LOGGER.error("Unauthorized: Invalid discord token");
-            } else {
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e2) {
-                    e2.printStackTrace();
-                }
-            }
-            return null;
-        }
-    }
-
-    private List<Message> parseMessages(String jsonResponse) {
-        List<Message> messages = new ArrayList<>();
-        Gson gson = new Gson();
-        JsonArray jsonArray = gson.fromJson(jsonResponse, JsonArray.class);
-        for (JsonElement element : jsonArray) {
-            JsonObject jsonObject = element.getAsJsonObject();
-            String id = jsonObject.get("id").getAsString();
-            String content = jsonObject.get("content").getAsString();
-            String author = jsonObject.get("author").getAsJsonObject().get("username").getAsString();
-            boolean isBot = jsonObject.get("author").getAsJsonObject().get("bot") != null;
-            boolean isWebhook = jsonObject.get("webhook_id") != null;
-            messages.add(new Message(id, content, author, isBot, isWebhook));
-        }
-        return messages;
-    }
-
-    private boolean isBotOrWebhook(Message message) {
-        return message.isBot() || message.isWebhook();
+        return content;
     }
 
     @EventListener
@@ -166,8 +86,10 @@ public class IRCModule extends ToggleModule {
             String trimmedMessage = message.substring(prefix.length());
             if (trimmedMessage.length() < 150) {
                 new Thread(() -> {
+                    String finalTrimmedMessage = trimmedMessage;
                     if (discordConfig.getValue()) {
-                        SocketWebhookManager.send(mc.getSession().getUsername(), trimmedMessage);
+                        finalTrimmedMessage = replaceMentions(finalTrimmedMessage);
+                        SocketWebhookManager.send(mc.getSession().getUsername(), finalTrimmedMessage);
                     }
                     chat.send(trimmedMessage);
                 }).start();
@@ -192,9 +114,9 @@ public class IRCModule extends ToggleModule {
                 }
             }
             Formatting nickColor = isDev ? Formatting.LIGHT_PURPLE : Formatting.WHITE;
-            ChatUtil.clientSendMessageRaw(Formatting.GRAY + "[" + Formatting.AQUA + "OvaqReborn" + Formatting.GRAY + "] " + nickColor + Formatting.BOLD + nick + ": " + text + Formatting.RESET);
+            if (discordConfig.getValue() && receivediscordConfig.getValue() && nick.equalsIgnoreCase("Server"))
+                ChatUtil.clientSendMessageRaw(Formatting.GRAY + "[" + Formatting.AQUA + "OvaqReborn" + Formatting.DARK_BLUE + "Discord" + Formatting.GRAY + "] " + nickColor + Formatting.BOLD + nick + ": " + text + Formatting.RESET);
+            else ChatUtil.clientSendMessageRaw(Formatting.GRAY + "[" + Formatting.AQUA + "OvaqReborn" + Formatting.GRAY + "] " + nickColor + Formatting.BOLD + nick + ": " + text + Formatting.RESET);
         }).start();
     }
-
-    private record Message(String id, String content, String author, boolean isBot, boolean isWebhook) {}
 }
