9d77d7a4e3b440812fae0f9c93d1822c3461e502
SilverCatKawaii <mikanmc77772@gmail.com>
2024-10-18 10:44:01 +0900

    安定してないと思うけどマインクラフトとDiscordの連携

--- src/main/java/net/shoreline/client/impl/module/client/IRCModule.java
+++ src/main/java/net/shoreline/client/impl/module/client/IRCModule.java
@@ -1,23 +1,38 @@
 package net.shoreline.client.impl.module.client;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import net.minecraft.util.Formatting;
+import net.shoreline.client.OvaqReborn;
 import net.shoreline.client.api.config.Config;
 import net.shoreline.client.api.config.setting.BooleanConfig;
+import net.shoreline.client.api.event.EventStage;
 import net.shoreline.client.api.event.listener.EventListener;
 import net.shoreline.client.api.module.ModuleCategory;
 import net.shoreline.client.api.module.ToggleModule;
+import net.shoreline.client.impl.event.TickEvent;
 import net.shoreline.client.impl.event.gui.chat.ChatMessageEvent;
 import net.shoreline.client.impl.event.network.SocketReceivedPacketEvent;
 import net.shoreline.client.socket.SocketChat;
 import net.shoreline.client.socket.SocketWebhookManager;
 import net.shoreline.client.util.chat.ChatUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Rom & Hypinohaizin
  */
 public class IRCModule extends ToggleModule {
     public static SocketChat chat = new SocketChat("wss://hack.chat/chat-ws", "OvaqRebornChat", mc.getSession().getUsername(), "");
-    //TODO:非同期処理にして修正 by hypinohoaizin
     Config<Boolean> discordConfig = new BooleanConfig("Discord", "make u lag when u send message. we will fix this later", false); // laggy maybe
 
     String[] devs = {
@@ -25,6 +40,8 @@ public class IRCModule extends ToggleModule {
             "7wp5",
             "Naa_Naa"
     };
+
+    private String lastMessageId = null;
 
     public IRCModule() {
         super("IRC", "global chat. prefix: @", ModuleCategory.CLIENT);
@@ -50,6 +67,82 @@ public class IRCModule extends ToggleModule {
         new Thread(() -> {
             chat.disconnect();
         }).start();
+    }
+
+    @EventListener
+    public void onTick(TickEvent event) {
+        if (event.getStage() != EventStage.PRE) {
+            return;
+        }
+        new Thread(() -> {
+            try {
+                String myId = "1296263865327288403";
+
+                new Thread(() -> {
+                    try {
+                        URL url = new URL("https://discord.com/api/v9/channels/" + myId + "/messages?limit=1");
+                        final String jsonResponse = getString(url);
+                        if (jsonResponse != null && !jsonResponse.isEmpty()) {
+                            List<Message> messages = parseMessages(jsonResponse);
+                            if (!messages.isEmpty() && !messages.get(0).id().equals(lastMessageId)) {
+                                lastMessageId = messages.get(0).id();
+                                ChatUtil.clientSendMessageRaw(Formatting.GRAY + "[" + Formatting.AQUA + "OvaqReborn" + Formatting.DARK_BLUE + "Discord" + Formatting.GRAY + "] " + Formatting.WHITE + Formatting.BOLD + messages.get(0).author() + ": " + messages.get(0).content() + Formatting.RESET);
+                            }
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            Thread.sleep(5000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            } catch (Exception e) {}
+        }).start();
+    }
+
+    private @NotNull String getString(URL url) throws IOException {
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Authorization", "MTI5NjYzNzgxNDk4NDY3NTQyMw.Gk9uJ7.QcH-4CWG484hqs3mIzSLtrwd0uN94XBE0HYrM8");
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder content = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+            con.disconnect();
+            return content.toString();
+        } catch (IOException e) {
+            if (con.getResponseCode() == 401) {
+                OvaqReborn.LOGGER.error("Unauthorized: Invalid discord token");
+            } else {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e2) {
+                    e2.printStackTrace();
+                }
+            }
+            return null;
+        }
+    }
+
+    private List<Message> parseMessages(String jsonResponse) {
+        List<Message> messages = new ArrayList<>();
+        Gson gson = new Gson();
+        JsonArray jsonArray = gson.fromJson(jsonResponse, JsonArray.class);
+        for (JsonElement element : jsonArray) {
+            JsonObject jsonObject = element.getAsJsonObject();
+            String id = jsonObject.get("id").getAsString();
+            String content = jsonObject.get("content").getAsString();
+            String author = jsonObject.get("author").getAsJsonObject().get("username").getAsString();
+            messages.add(new Message(id, content, author));
+        }
+        return messages;
     }
 
     @EventListener
@@ -90,4 +183,6 @@ public class IRCModule extends ToggleModule {
             ChatUtil.clientSendMessageRaw(Formatting.GRAY + "[" + Formatting.AQUA + "OvaqReborn" + Formatting.GRAY + "] " + nickColor + Formatting.BOLD + nick + ": " + text + Formatting.RESET);
         }).start();
     }
+
+    private record Message(String id, String content, String author) {}
 }
